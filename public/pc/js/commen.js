1909e990d70d73bfee906ed98bfa576e67fdba57
zjz <413492005@qq.com>
2018-01-12 21:32:26 +0800

    退出登录功能实现中...

--- public/pc/js/commen.js
+++ public/pc/js/commen.js
@@ -10,16 +10,38 @@
         NProgress.done();
     });
 
+    //判断是否登录
+    if(location.href.indexOf('login.html') < 0){
+        
+    }
+
+
+    //分类管理菜单收缩
     $('.second').prev().on('click', function () {
         $(this).next().slideToggle();
     });
-
+    //菜单按钮 点击收缩
     $('.icon_menu').on('click', function () {
         $('.lt_main').toggleClass('now');
         $('.lt_aside').toggleClass('now');
     });
-
+    //退出图标按钮
     $('.icon_out').on('click', function () {
         $('.myModal').modal('show');
-        console.log(111)
+    });
+
+    //点击退出 退出登录
+    $('.btn_logout').on('click',function () {
+        $.ajax({
+            type: 'get',
+            url: '/employee/employeeLogout',
+            datatype: 'json',
+            success:function(info) {
+                if(info.success){
+                    location.href = 'login.html';
+                }
+                //console.log(info);
+            }
+
+        });
     });
